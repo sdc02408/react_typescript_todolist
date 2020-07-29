311324a471163951030732565dc95f0709d6fda6
sdc02408 <sdc02408@naver.com>
2020-07-29 23:51:58 +0900

    app.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from '../logo.svg';
-import './App.css';
+import React, { useState, useCallback, ChangeEvent, KeyboardEvent } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { actions, RootState, selectTodoList, Todo } from '../features'
 
-function App() {
+const TodoEditor = () => {
+  const dispatch = useDispatch();
+  const [inputText,setInputText] = useState<string>('');
+
+  const handleText = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  },[])
+
+  const handleEnter = useCallback((e: KeyboardEvent<HTMLInputElement>) => {
+    if(inputText && e.keyCode ===13){
+      dispatch(actions.addTodos({
+        text:inputText,
+        isDone: false
+      }));
+      setInputText('');
+    }
+  },[dispatch,inputText])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+      type='text'
+      onChange={handleText}
+      onKeyDown={handleEnter}
+      value={inputText}
+      className='txt-input'
+      placeholder='써라'/>
     </div>
-  );
+  )
+}
+
+const TodoList = () => {
+  const dispatch = useDispatch();
+  const todoList = useSelector<RootState,Todo[]>(state => selectTodoList(state.todos));
+
+  return (
+    <ul>
+      {todoList.map((item: Todo) => (
+        <li key={item.id}>
+          <span className={item.isDone ? 'txt-complete' : ''}>
+            {item.text}
+          </span>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+
+const App = () => {
+  return(
+    <div className='container'>
+      <h1 className='title'>Todo List</h1>
+      <TodoEditor />
+      <TodoList />
+    </div>
+  )
 }
 
 export default App;
--- src/features/index.ts
+++ src/features/index.ts
@@ -25,9 +25,11 @@ export interface TodoList  {
 
 
 //createAction을 이용하여  addTodos라는 액션함수 생성.
-//createAction은 type을 인자로 받아 액션 함수를 만든다. 액션함수에 특정 값을 인자로 넣어 실행하면 그 값이 payload라는 것으로 포함된 액션 객체가 생성된다.
+//createAction은 type을 인자로 받아 액션 함수를 만든다.
+//액션함수에 특정 값을 인자로 넣어 실행하면 그 값이 payload라는 것으로 포함된 액션 객체가 생성된다.
 const actionPrefix = 'TODOS';
 const addTodos = createAction<object>(`%{actionPrefix}/add`);
+//addTodos({  id:'abc1', text:'hello',})   {types:'TODOS/add', payload:{id:'abc1'. text:'hello'}} addTodos함수를 실행하면 이와 같은 객체가 리턴된다.
 
 const reducers = {
   add:({list}: TodoList, {payload:{text,isDone}} : PayloadAction<Todo>) => {
@@ -41,12 +43,14 @@ const reducers = {
   }
 }
 
+
 const todoSlice = createSlice({
-  reducers,
-  initialState,
-  name:actionPrefix,
+  reducers,  //각 action type 별로 바뀔 상태값을 정의할 reducer객체
+  initialState, //초기상태
+  name:actionPrefix, //name:은 위와 actionPrefix값이 들어감. reducers 객체에 add라는 속성이 존재하면, 자동으로 actionType 생성
 })
 
+//createSelector 함수는 selector를 생성하는 함수로 memoizaation을 활용해 state 값을 효율적으로 가져올 수 있도록 한다.
 export const selectTodoList = createSelector(
   (state: TodoList) => state.list,
   (list:Todo[]) => list,

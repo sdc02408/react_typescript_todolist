21a0292e0977dcc5fac17ca8692a198a40fecb8a
sdc02408 <sdc02408@naver.com>
2020-08-01 23:47:10 +0900

    list표시

--- src/features/index.ts
+++ src/features/index.ts
@@ -15,42 +15,38 @@ export interface Todo {
 }
 
 //Todo가 들어잇는 배열 , list 속성이 있어.
-export interface TodoList  {
+
+export interface TodoList {
   list: Todo[];
 }
-
- const initialState: TodoList ={
-  list:[],
-}
-
+const initialState: TodoList = {
+  list: [],
+};
 
 //createAction을 이용하여  addTodos라는 액션함수 생성.
 //createAction은 type을 인자로 받아 액션 함수를 만든다.
 //액션함수에 특정 값을 인자로 넣어 실행하면 그 값이 payload라는 것으로 포함된 액션 객체가 생성된다.
+
 const actionPrefix = 'TODOS';
-const addTodos = createAction<object>(`%{actionPrefix}/add`);
-const toggleTodos = createAction<object>(`${actionPrefix}/toggle`)
+const addTodos = createAction<object>(`${actionPrefix}/add`);
+const toggleTodos = createAction<object>(`${actionPrefix}/toggle`);
 
 //addTodos({  id:'abc1', text:'hello',})   {types:'TODOS/add', payload:{id:'abc1'. text:'hello'}} addTodos함수를 실행하면 이와 같은 객체가 리턴된다.
 
 const reducers = {
-  add:({list}: TodoList, {payload:{text,isDone}} : PayloadAction<Todo>) => {
+  add: ({ list }: TodoList, { payload: { text, isDone } }: PayloadAction<Todo>) => {
     const newTodo: Todo = {
       id: generateRandomStr(5),
-      text:text.toString(),
+      text: text.toString(),
       isDone
     };
     list.push(newTodo);
   },
-
-  toggle: ({list}: TodoList, {payload: {id,isDone}}: PayloadAction<Todo>) => {
-    const targetIndex = list.findIndex((item:Todo) => item.id === id)
-
-    list[targetIndex].isDone= !isDone;
+  toggle: ({ list }: TodoList, { payload: { id, isDone } }: PayloadAction<Todo>) => {
+    const targetIndex = list.findIndex((item: Todo) => item.id === id);
+    list[targetIndex].isDone = !isDone;
   },
-
-}
-
+};
 
 const todoSlice = createSlice({
   reducers,  //각 action type 별로 바뀔 상태값을 정의할 reducer객체

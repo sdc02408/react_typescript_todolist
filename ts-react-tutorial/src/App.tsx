2679dfca0b287f6a043c26f6a67b427d4abd59a8
sdc02408 <sdc02408@naver.com>
2020-08-05 23:26:11 +0900

    기초부터

---
+++ ts-react-tutorial/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import Counter from './Counter'
+import MyForm from './MyForm'
+
+function App() {
+  const onSubmit = (form: {name:string; description: string}) => {
+    console.log(form);
+  }
+  
+  return (
+   <MyForm onSubmit={onSubmit} />
+  );
+}
+
+export default App;
